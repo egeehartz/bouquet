686da4eca5bb585bb8ca63b506bb1e827df87332
Emily Hartzell <emily.g.hartzell@gmail.com>
2020-08-17 15:55:43 -0500

    added retailer directory and files

--- scripts/flowers/FlowerProvider.js
+++ scripts/flowers/FlowerProvider.js
@@ -3,7 +3,7 @@ let flowers = []
 
 
 export const getFlowers = () => {
-    return fetch("http://localhost:3000/flowers")
+    return fetch("http://localhost:3000/flowers?_expand=retailer")
         .then(resp => resp.json())
         .then(parsedRes => {
             flowers = parsedRes
